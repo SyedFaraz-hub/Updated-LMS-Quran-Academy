8ea3ccaeed6615da51777ab60d6c757fa4bb558f
faraz <farazshah116@gmail.com>
2021-09-23 02:53:01 +0500

    footer text change

--- Components/Footer.php
+++ Components/Footer.php
@@ -1,3 +1,3 @@
 <div class=" footer">
-    <span>Copyright &copy; 2021 Babul-Quran. All Rights Reserved.</span>
+    <span>Copyright &copy; 2021 Babul-Quran. All Rights Reserved, Powered by <a class="underlineremove-and-colorwhite markcoder-hover" href="https://markcoders.com/">MarkCoders</a></span>
 </div>
